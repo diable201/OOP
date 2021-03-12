e08baa32de6650acbe4bdcc3db9a65f99116d38f
Sanzhar <diable201@protonmail.com>
2021-03-13 00:26:45 +0600

    Refactoring

--- Practice/src/Serialization.java
+++ Practice/src/Serialization.java
@@ -1,14 +1,10 @@
 import java.io.*;
 
 public class Serialization {
-
    public static void main(String[] args) throws IOException, ClassNotFoundException {
-
        FileOutputStream fos = new FileOutputStream("info.out");
        ObjectOutputStream oos = new ObjectOutputStream(fos);
-
        StudentSer studentInfo = new StudentSer("Rias", "My Secret");
-
        oos.writeObject(studentInfo);
        oos.flush();
        oos.close();
