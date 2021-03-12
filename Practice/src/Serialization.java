1b46eb6e880fb39a53b728676c81c0a0d1888c9d
Sanzhar <diable201@protonmail.com>
2021-03-13 00:20:25 +0600

    Own Serialization Example

---
+++ Practice/src/Serialization.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Serialization {
+
+   public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+       FileOutputStream fos = new FileOutputStream("info.out");
+       ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+       StudentSer studentInfo = new StudentSer("Sanzhar", "My Secret");
+
+       oos.writeObject(studentInfo);
+       oos.flush();
+       oos.close();
+   }
+}
---
+++ Practice/src/StudentSer.java
@@ -0,0 +1,59 @@
+import java.util.Base64;
+import java.io.*;
+
+
+public class StudentSer implements Externalizable {
+    private String name;
+    private String secret;
+
+    public StudentSer() {}
+
+    public StudentSer (String name, String secret) {
+        this.name = name;
+        this.secret = secret;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(this.getName());
+        out.writeObject(this.encryptString(this.getSecret()));
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws ClassNotFoundException, IOException {
+        name = (String) in.readObject();
+        secret = this.decryptString((String) in.readObject());
+    }
+
+    private String encryptString(String data) {
+        String encryptedData = Base64.getEncoder().encodeToString((data.getBytes()));
+        System.out.println("Encyprion is succesfull: " + encryptedData);
+        return encryptedData;
+    }
+ 
+    private String decryptString(String data) {
+        String decryptedData = new String(Base64.getDecoder().decode(data));
+        System.out.println("Decryption is succesfull: " + decryptedData);
+        return decryptedData;
+    }
+    
+    public String toString() {
+    	return "Name: " + name + " Secret: " + secret;
+    }
+}
