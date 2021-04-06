fbd316d90ea1498c2d3bd4fdcca4c12733ea5a54
Sanzhar <diable201@protonmail.com>
2021-04-06 20:58:48 +0600

    Comparators

---
+++ Practice/src/IdComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class IdComparator implements Comparator<Product> {
+
+	@Override
+	public int compare(Product o1, Product o2) {
+		if (o1.id > o2.id) return 1;
+		if (o1.id < o2.id) return -1;
+		return 0;
+	}
+
+}
---
+++ Practice/src/Subproduct.java
@@ -0,0 +1,26 @@
+public class Subproduct extends Product {
+	
+	int count;
+	
+	public Subproduct() {
+		super();
+	}
+
+	public Subproduct(String name, int id, double price, int count) {
+		super(name, id, price);
+		this.count = count;
+	}
+	
+	@Override
+	public int compareTo(Product o) {
+		Subproduct sp = (Subproduct)o;
+		if (this.count > sp.count) return 1;
+		if (this.count < sp.count) return -1;
+		return super.compareTo(sp);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " [count=" + count + "]";
+	}
+}
